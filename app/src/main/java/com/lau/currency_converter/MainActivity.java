cf322f50f1dfaf4613fbc58cdb3ac52a8bd35759
IbrahimSrour <srouribrahim0@gmail.com>
2022-04-03 09:09:14 +0300

    Fixed errors

--- app/src/main/java/com/lau/currency_converter/MainActivity.java
+++ app/src/main/java/com/lau/currency_converter/MainActivity.java
@@ -3,6 +3,7 @@ package com.lau.currency_converter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -15,6 +16,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         changePage.findViewById(R.id.changePageButton);
         updatedRate.findViewById(R.id.updatedRate);
-
+        changePage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, Page2.class));
+            }
+        });
     }
 }
