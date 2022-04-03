d6836d33227b6c0f656c30e7453fad92effc99b7
IbrahimSrour <srouribrahim0@gmail.com>
2022-04-03 07:33:56 +0300

    Layouts defined

--- app/src/main/java/com/lau/currency_converter/MainActivity.java
+++ app/src/main/java/com/lau/currency_converter/MainActivity.java
@@ -3,12 +3,18 @@ package com.lau.currency_converter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button changePage;
+    private EditText updatedRate;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        changePage.findViewById(R.id.changePageButton);
+        updatedRate.findViewById(R.id.updatedRate);
+
     }
 }
